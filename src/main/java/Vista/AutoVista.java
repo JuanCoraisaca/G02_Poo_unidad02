fd00921d52374044f22ea6e4d59738e1799da56a
usuario <usuario@localhost>
2022-05-06 08:43:21 -0500

    Deber POO04 final

--- src/main/java/Vista/AutoVista.java
+++ src/main/java/Vista/AutoVista.java
@@ -22,8 +22,20 @@ public class AutoVista {
         var autoControlador = new AutoControlador();
         autoControlador.crear(2010, "ADF 674", 01, "Negro", "Juan", 016674, 2889, "Miraflores", 001);
         autoControlador.crear(2015, "GAP 543", 02, "Blanco", "Pedro", 016675, 4060, "Larga", 002);
+        
+        
+        var auto1 = new Auto(2010,"ADF 674", 01, "Negro");
+        var auto2 = new Auto(2015, "GAP 543", 02, "Blanco");
+        
+        System.out.println("La edad del auto es: " + auto1.calcularYears(2022));
+        System.out.println("El auto es taxi? " + auto1.esTaxi());
+        
+        System.out.println(".........................");
+        
+        System.out.println("La edad del auto es: " + auto2.calcularYears(2022));
+        System.out.println("El auto es taxi? " + auto2.esTaxi());
 
-        System.out.println("Lista de Autos");
+        System.out.println("Lista de Autos........");
         for (Auto auto : autoControlador.listar()) {
             System.out.println(auto.toString());
 
